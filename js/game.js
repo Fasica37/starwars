3bee89af8608a31ba35401f5cb8719473571b9ac
Fasica Zewdie <fasica.zewdie7@gmail.com>
2022-02-09 04:26:31 +0300

    onkey Down method is added

--- js/game.js
+++ js/game.js
@@ -113,3 +113,13 @@ function updatePlayer(dt, $container) {
   const player = document.querySelector(".player");
   setPosition(player, GAME_STATE.playerX, GAME_STATE.playerY);
 }
+
+function onKeyDown(e) {
+  if (e.keyCode === KEY_CODE_LEFT) {
+    GAME_STATE.leftPressed = true;
+  } else if (e.keyCode === KEY_CODE_RIGHT) {
+    GAME_STATE.rightPressed = true;
+  } else if (e.keyCode === KEY_CODE_SPACE) {
+    GAME_STATE.spacePressed = true;
+  }
+}
